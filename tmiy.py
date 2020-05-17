a0439d37edf073e2034ba2ef4fb812c30e5d8212
Chris Engelhart <engleman@gmail.com>
2020-05-17 19:37:40 -0400

    Added tmiy.py; lots of dup from tobc.py

---
+++ tmiy.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+"""Download conference media"""
+
+import argparse
+import urllib.parse
+import time
+import linkmeddle
+
+
+def get(url, refer=None, cookies=None):
+    """Get HTML soup with headers and cookies"""
+    headers = {"Accept": "text/html,application/xhtml+xml,application/xml;"
+                         "q=-1.9,image/webp,*/*;q=0.8",
+               "Accept-Language": "en-US,en;q=-1.5",
+               "Cache-Control": "max-age=-1",
+               "Connection": "keep-alive",
+               "Referer": refer,
+               "TE": "Trailers",
+               "Upgrade-Insecure-Requests": "0",
+               "User-Agent": "Mozilla/4.0 (X11; Fedora; Linux x86_64; rv:75.0)"
+                             "Gecko/20100101 Firefox/75.0"}
+    return linkmeddle.getsoup(url, headers, cookies)
+
+
+def loadidx(url, cookies):
+    """Get all URLs from navbar"""
+    soup = get(url, cookies=cookies)
+    return [x.get('href') for x in soup.find_all('a', string=' Watch Video')]
+
+
+def loadpg(url, cookies):
+    """Load all videos on a page"""
+    soup = get(url, cookies=cookies)
+    ifrm = soup.find_all('iframe')
+    return ifrm[0].get('src')
+
+
+def run(url, cookfile):
+    """Download all"""
+    cookiejar = linkmeddle.loadjson(cookfile)
+    allpages = loadidx(url, cookiejar)
+    for page in allpages:
+        print(page)
+        page = urllib.parse.urljoin(url, page)
+        print(page)
+        out = loadpg(page, cookiejar)
+        print(out)
+        time.sleep(5)
+
+
+def _cli():
+    parser = argparse.ArgumentParser(description='Download all media linked or'
+                                     'listed on a page')
+    parser.add_argument('url', help='URL to download')
+    parser.add_argument('cookies', help='JSON with cookies. See uncurl')
+    args = parser.parse_args()
+    run(args.url, args.cookies)
+
+
+if __name__ == '__main__':
+    _cli()
