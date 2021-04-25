17cce00d2105b407b5d2b5a7db8c60fcbcdea3cb
Chris Engelhart <engleman@gmail.com>
2021-04-25 07:59:20 -0400

    Enhancements to readlogs.py
    16-32 second range on errors
    Output list of successful playlists and failed anything, including
    accounting for errors and ignoreerrrors
    CSV output
    
    Part of #15
    next steps to test and attempt human analysis with readdb.py

--- apiqueue/readlogs.py
+++ apiqueue/readlogs.py
@@ -5,7 +5,7 @@ import datetime
 import uuid
 import json
 import collections
-#import csv
+import csv
 import click
 
 
@@ -15,14 +15,15 @@ def pulldirinfo(tdr):
     for item in tdr.iterdir():
         luuid = uuid.UUID(item.name)
         mtime = datetime.datetime.fromtimestamp(item.stat().st_mtime)
-        with item.open() as f:
-            info = json.load(f)
+        with item.open() as file_hand:
+            info = json.load(file_hand)
         # TODO add support for non-url
         url = info.get('url')
         backend = info.get('backend', ['ytdl', None])
         data = info.get('data')
         error = info.get('error')
-        ok = (bool(data) and not bool(error))
+        ignoreerrors = info.get('ignoreerrors')
+        dl_ok = (bool(data) and not bool(error))
         if not data:
             data = {}
         if not backend[1]:
@@ -32,26 +33,27 @@ def pulldirinfo(tdr):
                         'updated': mtime,
                         'url': url,
                         'backend': backend,
-                        'success': ok,
+                        'success': dl_ok,
                         'error': error,
-                        'type': mytyp})
+                        'type': mytyp,
+                        'ignoreerrors': ignoreerrors})
     return outlist
 
 def greplogs(logf, whendct):
     """Grep a log file for interesting minutes"""
-    greps = collections.defaultdict(list)
+    greps = collections.defaultdict(set)
     result = {k: [] for k in whendct.keys()}
-    for k, v in whendct.items():
-        for thismin in [-31, 31]:
-            newtime = v + datetime.timedelta(seconds=thismin)
-            minstring = newtime.isoformat(sep=' ')[0:16]
-            greps[minstring].append(k)
+    for key, val in whendct.items():
+        for thismin in [-8, 0, 8]:
+            newtime = val + datetime.timedelta(seconds=thismin)
+            minstring = newtime.isoformat(sep=' ')[0:17]
+            greps[minstring].add(key)
     with logf.open() as myfh:
         while True:
             fulline = myfh.readline()
             if not fulline:
                 break
-            for tgt in greps[fulline[1:17]]:
+            for tgt in greps[fulline[1:18]]:
                 result[tgt].append(fulline.strip())
     return result
 
@@ -63,28 +65,55 @@ def addlogdetails(dirdata, logfile):
     """try to add failure details from log"""
     times = logtimes(dirdata)
     details = greplogs(logfile, times)
-    for x in dirdata:
-        x['logdetails'] = details.get(x['logid'])
+    for myitem in dirdata:
+        myitem['logdetails'] = details.get(myitem['logid'])
 
 # TODO attempt to read flask.log ?
 
-# TODO analysis such as output playlists only
-# TODO output current failures (i.e. if it failed before but works now don't output)
 
 
 @click.command()
 @click.argument('dirname')
-@click.argument('cellog')
-@click.argument('outcsv')
-def logs2csv(dirname, cellog, outcsv):
+@click.option('-l', '--log-file', help="Celery log file")
+@click.option('-c', '--csv-file', help="CSV output file")
+@click.option('-s', '--success', help="Success output file (pl only)")
+@click.option('-f', '--failure', help="Failure output file (all)")
+def logs2csv(dirname, log_file, csv_file, success, failure):
     """Read a local log directory and log file and output a CSV"""
     print('Reading directory...')
     mydirinfo = pulldirinfo(Path(dirname))
-    for x in mydirinfo:
-        print(x)
-    addlogdetails(mydirinfo, Path(cellog))
-    for x in mydirinfo:
-        print(x)
+    if log_file:
+        print('Reading log file...')
+        addlogdetails(mydirinfo, Path(log_file))
+    if csv_file:
+        print('Writing CSV file...')
+        with open(csv_file, 'w', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=mydirinfo[0].keys())
+            writer.writeheader()
+            for myitem in mydirinfo:
+                writer.writerow(myitem)
+    if success or failure:
+        print('Analyzing...')
+        mydirinfo.sort(key=lambda x: x['updated'])
+        simple_list = {}
+        for myitem in mydirinfo:
+            if myitem['ignoreerrors']:
+                if (myitem['backend'], myitem['url']) in simple_list:
+                    continue
+                simple_list[(myitem['backend'], myitem['url'])] = (myitem['type'], False)
+                continue
+            simple_list[(myitem['backend'], myitem['url'])] = (myitem['type'], myitem['success'])
+        if success:
+            with open(success, 'w') as succ_f:
+                for key, val in simple_list.items():
+                    if val[1] and val[0] != 'video':
+                        print('\t'.join(key), file=succ_f)
+        if failure:
+            with open(failure, 'w') as fail_f:
+                for key, val in simple_list.items():
+                    if not val[1]:
+                        print('\t'.join(key), file=fail_f)
+
 
 if __name__ == '__main__':
     logs2csv()
