a4fff58693ae250dad703634816d66a3ea62b299
Chris Engelhart <engleman@gmail.com>
2021-04-29 15:31:33 -0400

    Finishish si, but not really

--- apiqueue/si.py
+++ apiqueue/si.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+# NOTE this is essentially a sample
 
 """Download files from JSON"""
 
@@ -7,6 +8,8 @@
 import argparse
 import warnings
 import json
+import urllib
+from posixpath import join as urljoin
 import linkmeddle
 #import requests_cache
 #import datetime
@@ -17,7 +20,6 @@ import linkmeddle
 #import random
 #import re
 #import os
-#import urllib
 
 
 QUAL = ['4K', 'High', 'Med', 'Low']
@@ -83,22 +85,67 @@ def json_dl(jsonf, base):
 #    return total
 
 def download(info):
+    error = None
     mytype = info.get('type', 'playlist')
-    url = info.get('burl')
+    url = info.get('url')
+    url_split = urllib.parse.urlparse(url)
+    url_scheme = url_split.scheme
+    assert url_scheme in ['http', 'https']
+    site_base = url_split.netloc
+    url_path = url_split.path
+    assert not url_split.params
+    assert not url_split.query
+    assert not url_split.fragment
+    assert not url_split.username
+    assert not url_split.password
+    assert urlsplit.hostname == site_base
+    assert not url_split.port
     if mytype == 'video':
-        # TODO download one to proper place
-        # what should URL be???
-        return {}
+        warnings.warn('SI: Not downloading {}'.format(url))
+        details = info.get('details')
+        assert details
+        # TODO download one to proper place SICFG['DIR] . SICFG['VPATH']['PPATH']
+        real_url = None
+        return {'url': url,
+                'backend': ['si', 'video'],
+                'data': {'real_url': real_url,
+                         'details': details,
+                'error': error,
+                'recurse': False,
+                'retrieved': True,
+                'type': mytype,
+                'partial': False}
     assert mytype == 'playlist'
     recurse = info.get('recurse', 'async')
     assert recurse in ['async', False]  # sync and internal not supported yet
-    # TODO save versioned/dated JSON file
     # TODO allow passing in site instead of JSON
     vids = linkmeddle.get_json(url)
-    # TODO add passback info and return
+    # TODO save versioned/date JSON file in SICFG['DIR']
+    entries = [{'url': urllib.parse.urlunsplit([url_scheme,
+                                                site_base,
+                                                urljoin(SICFG['PPATH'],
+                                                        '{} {}.jpg'.format(x['siteName'], x['videoName']),
+                                                None,
+                                                None]),
+                'backend': ['si', 'playlist'],
+                'retrieved': False,
+                'details': x} for x in vids]
+    if entries and recurse == 'async':
+        recurse_status = 'prepared'
+    return {'url': url,
+            'backend': ['si', 'playlist'],
+            'data': {'entries': entries},
+            'error': error,
+            'recurse': recurse,
+            'recurse_status': recurse_status,
+            'type': mytype
+            'partial': False}
+
+
 
 
 def set_config(cfg_info):
+    """Send in a dictionary with DIR, VPATH, and PPATH"""
     global SICFG
     SICFG = cfg_info.copy()
 
