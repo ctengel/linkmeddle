b6493face255e78979f023d25891888747d72bf7
Chris Engelhart <engleman@gmail.com>
2021-05-12 07:39:00 -0400

    Refactor del.py to allow csv/user mode
    Part of #55

--- del.py
+++ del.py
@@ -1,20 +1,48 @@
 #!/usr/bin/env python3
 
+"""Tools for deleting many files and metadata from a youtube-dl library"""
+
 
 from pathlib import Path
 import click
 
+
+def delone(item, verbose=True):
+    """Delete one file given a Path object"""
+    if verbose:
+        print(item, item.exists())
+    if item.exists():
+        item.unlink()
+    if verbose:
+        print(item, item.exists())
+
+def txt2paths(textfile):
+    """Read a text file and output a list of Path objects"""
+    with open(textfile) as filehandle:
+        paths = [Path(x.strip('\n')) for x in filehandle]
+    return paths
+
+def csv2paths(csvfile, users):
+    """Return paths associated with users from a text file"""
+    # TODO implement to close #55
+    assert False
+    return []
+
 @click.command()
-@click.argument('textfile')
-def delmany(textfile):
-    """Delete many files using a text file"""
-    with open(textfile) as fh:
-        paths = [Path(x.strip('\n')) for x in fh]
+@click.option('-t', '--textfile', help='text file with files to delete')
+@click.option('-c', '--csvfile', help='CSV output from readdb.py')
+@click.argument('users', nargs=-1)
+def delmany(textfile=None, csvfile=None, users=None):
+    """Delete many files using a text file or csv with users"""
+    assert bool(csvfile and users) != bool(textfile)
+    assert bool(csvfile) == bool(users)
+    if textfile:
+        paths = txt2paths(textfile)
+    else:
+        paths = csv2paths(csvfile, users)
     for item in paths:
-        print(item, item.exists())
-        if item.exists():
-            item.unlink()
-        print(item, item.exists())
+        delone(item)
+
 
 if __name__ == '__main__':
     delmany()
