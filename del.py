f42b6c4465901c963034986f3b831b52010c6081
Chris Engelhart <engleman@gmail.com>
2021-05-20 06:22:54 -0400

    del.py finds playlists and non-yt
    Closes #57
    Still needs testing

--- del.py
+++ del.py
@@ -24,12 +24,13 @@ def txt2paths(textfile):
         paths = [Path(x.strip('\n')) for x in filehandle]
     return paths
 
-def csv2paths(csvfile, users, directory='.'):
+def csv2paths(csvfile, users, directory='.', searchparam='uploader', backend='youtube'):
     """Return paths associated with users from a text file"""
     mydir = Path(directory)
     dirlist = list(mydir.iterdir())
     with open(csvfile, newline='') as csvfh:
-        interest = [x for x in csv.DictReader(csvfh) if x['uploader'] in users and x['extractor_key'] == 'youtube']
+        interest = [x for x in csv.DictReader(csvfh)
+                    if x[searchparam] in users and x['extractor_key'] == backend]
     dellist = []
     for thisfile in interest:
         if thisfile['mediafile'] and thisfile['ijfn'] and not thisfile['possiblefiles'] and thisfile['mediafile'].startswith(thisfile['title']) and thisfile['ijfn'].startswith(thisfile['title']):
@@ -39,7 +40,8 @@ def csv2paths(csvfile, users, directory='.'):
             dellist.append(mydir.joinpath(thisfile['ijfn']))
             dellist.append(mydir.joinpath(thisfile['mediafile']))
             for ckone in dirlist:
-                if ckone.name.startswith(base) and ckone.name not in [thisfile['ijfn'], thisfile['mediafile']]:
+                if ckone.name.startswith(base) and ckone.name not in [thisfile['ijfn'],
+                                                                      thisfile['mediafile']]:
                     warnings.warn('additional {}'.format(ckone))
                     dellist.append(ckone)
             continue
@@ -54,16 +56,28 @@ def csv2paths(csvfile, users, directory='.'):
 @click.option('-c', '--csvfile', help='CSV output from readdb.py')
 @click.option('-p', '--pretend', help='Don\'t actually delete, just pretend', is_flag=True)
 @click.option('-d', '--directory', help='Directory for CSV mode')
-@click.argument('users', nargs=-1)
-def delmany(textfile=None, csvfile=None, pretend=False, directory='.', users=None):
+@click.option('-b', '--backend', help='YTDL extractor to search/delete on', default='youtube')
+@click.option('--playlists/--users', '-p/-u', default=False, help='Users or playlists')
+@click.argument('userspls', nargs=-1)
+def delmany(textfile=None,
+            csvfile=None,
+            pretend=False,
+            directory='.',
+            backend='youtube',
+            playlists=False,
+            userspls=None):
     """Delete many files using a text file or csv with users"""
-    assert bool(csvfile and users) != bool(textfile)
-    assert bool(csvfile) == bool(users)
+    assert bool(csvfile and userspls) != bool(textfile)
+    assert bool(csvfile) == bool(userspls)
 
     if textfile:
         paths = txt2paths(textfile)
     else:
-        paths = csv2paths(csvfile, users, directory)
+        if playlists:
+            searchparam='playlist'
+        else:
+            searchparam='uploader'
+        paths = csv2paths(csvfile, userspls, directory, backend=backend, searchparam=searchparam)
     for item in paths:
         delone(item, pretend=pretend)
 
