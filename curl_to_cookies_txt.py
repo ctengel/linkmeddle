00c22f4832a7b21695f7c126124c1af36977ed8f
Chris Engelhart <engleman@gmail.com>
2023-08-25 16:35:45 -0400

    Added CLI for curl_to_cookies_txt
    Closes #77

--- curl_to_cookies_txt.py
+++ curl_to_cookies_txt.py
@@ -1,7 +1,20 @@
+#!/usr/bin/env python3
+
+"""Tools to deal with cookiex.txt files
+
+See cli() function for CLI doc
+"""
+
 import http.cookiejar
 import http.cookies
+import sys
 
-def convert_header_to_cookies_txt(header_string, output_file):
+def convert_header_to_cookies_txt(header_string: str, output_file: str) -> None:
+    """Converts cookies header to cookies.txt
+
+    header_string is the HTTP Cookie: header value string from client request
+    output_file is the name of a new netscape cookies.txt to generate"""
+
     # Create a new cookie jar
     cookie_jar = http.cookiejar.MozillaCookieJar()
 
@@ -9,7 +22,7 @@ def convert_header_to_cookies_txt(header_string, output_file):
     cookies = http.cookies.SimpleCookie()
     cookies.load(header_string)
     for key, morsel in cookies.items():
-        print(key, morsel)
+        #print(key, morsel)
         # Create a Cookie object and add it to the jar
         cookie = http.cookiejar.Cookie(
             version=0,
@@ -31,6 +44,26 @@ def convert_header_to_cookies_txt(header_string, output_file):
             rfc2109=False
         )
         cookie_jar.set_cookie(cookie)
-    print(cookie_jar)
+    #print(cookie_jar)
     # Save the cookies to a cookies.txt file
     cookie_jar.save(output_file, ignore_discard=True)
+
+def cli():
+    """CLI to take curl and generate cookies.txt
+
+    First arg is the cookies.txt filename
+    Rest of args is a curl command
+    Can use firefox "copy to CURL" feature
+    """
+    cookiefile = sys.argv[1]
+    assert sys.argv[2] == 'curl'
+    for arg in sys.argv[3:]:
+        if arg.startswith('Cookie: '):
+            cookieinput = arg.removeprefix('Cookie: ')
+            #print(cookieinput, cookiefile)
+            convert_header_to_cookies_txt(cookieinput, cookiefile)
+            return
+    assert False
+
+if __name__ == '__main__':
+    cli()
