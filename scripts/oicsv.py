8fc2edf8ac059579f24ffece0fae8eee1bfdd6fc
Chris Engelhart <engleman@gmail.com>
2025-10-29 21:35:52 -0400

    move oicsv to simpler oi upload_remote call
    part of #79
    
    requires oi e660bf9ac2a41713b70d2ee3574c16b136981c95

--- scripts/oicsv.py
+++ scripts/oicsv.py
@@ -5,11 +5,8 @@
 import pathlib
 import csv
 import time
-import warnings
-import tempfile
 import os
 import random
-import requests.exceptions
 from obj_idx import client as oic
 import linkmeddle
 
@@ -26,23 +23,10 @@ def get_blog(blog_url, bucket=BUCKET):
     with csvpath.open(newline='') as cfh:
         cdr = csv.DictReader(cfh)
         for line in cdr:
-            # Check to see if exists first
-            filez = oicl.search_files({'url': line['source']})
-            if len(filez) == 1 and filez[0].info['file_object']['completed'] and filez[0].info['file_object']['obj_size'] > 1024*1024:
-                print(f"already got {line['source']}")
-                continue
-            with tempfile.NamedTemporaryFile() as temp:
-                try:
-                    digest, mime = oic.simple_download(line['source'], temp.name)
-                except requests.exceptions.HTTPError as excp:
-                    warnings.warn(str(excp))
-                    continue
-                if pathlib.Path(temp.name).stat().st_size < 1024*1024:
-                    warnings.warn(f"{line['source']} too small!")
-                    continue
-                tags = {'mblr-blog-file': csvpath.stem,
-                        'mblr-post-file': pathlib.PurePath(line['target']).stem}
-                fileobj = oic.upload(temp.name, oicl, bucket, tags, digest, mime, line['source'])
+            tags = {'mblr-blog-file': csvpath.stem,
+                    'mblr-post-file': pathlib.PurePath(line['target']).stem}
+            # TODO consider 'target' as key_hint
+            fileobj = oic.upload_remote(line['source'], oicl, bucket, extra=tags, catch_dl_err=True)
             print(line['source'], line['target'], fileobj.uuid)
             time.sleep(random.randint(30,60))
 
