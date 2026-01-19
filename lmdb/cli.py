545aeaff9857904bda9fce45c8dc7519ae6e0532
ctengel <50063864+ctengel@users.noreply.github.com>
2026-01-19 07:11:49 -0500

    actual dlp import

---
+++ lmdb/cli.py
@@ -0,0 +1,30 @@
+import datetime
+from . import models, xform
+
+DOCO = ''
+
+with open(DOCO) as f:
+    dlp = models.PlaylistDLP.model_validate_json(f.read())
+
+print(dlp)
+
+native = xform.pl_dlp2lm(dlp)
+
+print(native)
+
+summary = xform.full2sum(native)
+
+print(summary)
+
+stats = xform.full2stats(native, 1)
+
+print(stats)
+
+sched = models.PlaylistSched(extractor_id=summary.extractor_id, id=summary.id, next_run=datetime.date.today(), freq_days=0, input_prams={}, webpage_url=summary.webpage_url, runs=[])
+
+sched, existing, stats = xform.add_new_run(sched, [], stats)
+
+print(sched)
+print(existing)
+print(stats)
+
--- lmdb/xform.py
+++ lmdb/xform.py
@@ -81,7 +81,11 @@ def add_new_run(schedule: models.PlaylistSched,
                                                     models.PlaylistStats]:
     """Go through motions of adding a new run stats summary"""
     existing = sort_runs(existing)
-    new = rum_deltas(existing[-1], new)
+    if existing:
+        new = rum_deltas(existing[-1], new)
+    else:
+        new.different = True
+        new.interval = 0
     existing.append(new)
     new_freq = rec_adjust_freq(existing[-3:])
     if new_freq and new_freq != schedule.freq_days:
@@ -91,7 +95,7 @@ def add_new_run(schedule: models.PlaylistSched,
 
 def entry2text(entry: models.VidFull) -> str:
     """Change a pl entry into single unique string"""
-    # TODO implement
+    return entry.id
 
 def pl2txt(entries: list[models.VidFull]) -> str:
     """Change playlist entries into a string"""
@@ -99,13 +103,15 @@ def pl2txt(entries: list[models.VidFull]) -> str:
 
 def pl_hash(entries: list[models.VidFull]) -> bytes:
     """Hash a playlist"""
+    # TODO consider using summary instead of full as input
     hash_object = hashlib.sha256()
     hash_object.update(pl2txt(entries).encode())
     return hash_object.digest()
 
 def newest(entries: list[models.VidFull]) -> models.VidFull:
     """Find newest playlist entry"""
-    # TODO implement
+    # TODO handle empty list
+    return sorted(entries, key=lambda x: x.upload_date, reverse=True)[0]
 
 def full2stats(inputpl: models.PlaylistFull, download_count: int) -> models.PlaylistStats:
     """Convert a 'full' LM-Native playlist into stats
@@ -124,10 +130,11 @@ def full2stats(inputpl: models.PlaylistFull, download_count: int) -> models.Play
                                 output_params={},  # new func arg??? optional?
                                 timestamp=datetime.datetime.now(),  # allow arg override?
                                 newest_item=newest(inputpl.entries).upload_date,
-                                different=None,  # feed it to him later
-                                interval=None)  # feed it to him later
+                                different=True,  # TODO feed it to him later
+                                interval=0)  # TODO feed it to him later
 
 def failed_stat() -> models.PlaylistStats:
+    """Create a 'failed' playlist stat entry"""
     return models.PlaylistStats(playlist_count=0,
                                 entries_hash=pl_hash([]),
                                 success=False,
@@ -135,16 +142,53 @@ def failed_stat() -> models.PlaylistStats:
                                 timestamp=datetime.datetime.now())
 
 def full2sum(inputpl: models.PlaylistFull) -> models.PlaylistSum:
+    """Summarize playlist"""
     # clener way to copy common fields?
     # validate count
     return models.PlaylistSum(id=inputpl.id,
                               title=inputpl.title,
                               modified_date=inputpl.modified_date,
                               webpage_url=inputpl.webpage_url,
-                              playlist_count=inputpl.playlist_count,
+                              playlist_count=inputpl.playlist_count,  # validate
                               channel=inputpl.channel.channel_url,  # is this right?
                               entries=[entry2text(x) for x in inputpl.entries],
-                              extractor_id=inputpl.extractor.extractor_key)  # is this right?
+                              extractor_id=inputpl.extractor.extractor)  # is this right?
 
 def pl_dlp2lm(dlpin: models.PlaylistDLP) -> models.PlaylistFull:
-    pass
+    """Raw DLP playlist to LM-native playlist"""
+    return models.PlaylistFull(id=dlpin.id,
+                               title=dlpin.title,
+                               modified_date=datetime.datetime.fromtimestamp(dlpin.epoch),  # TODO
+                               webpage_url=dlpin.webpage_url,
+                               playlist_count=dlpin.playlist_count,  # TODO
+                               channel=models.UlChan(channel_id=dlpin.channel_id,
+                                                     uploader_id=dlpin.uploader_id,
+                                                     uploader=dlpin.uploader,
+                                                     channel_url=dlpin.channel_url,
+                                                     uploader_url=dlpin.uploader_url),
+                               entries=[models.VidFull(channel=models.UlChan(channel_id=x.channel_id,
+                                                                             uploader_id=x.uploader_id,
+                                                                             uploader=x.uploader,
+                                                                             channel_url=x.channel_url,
+                                                                             uploader_url=x.uploader_url),
+                                                       description=x.description,
+                                                       id=x.id,
+                                                       title=x.title,
+                                                       webpage_url=x.webpage_url,
+                                                       duration=x.duration,
+                                                       ext=x.ext,
+                                                       format=x.format,
+                                                       height=x.height,
+                                                       width=x.width,
+                                                       extractor=models.DLPIE(extractor_key=x.extractor_key,
+                                                                              extractor=x.extractor),
+                                                       categories=x.categories,
+                                                       is_live=x.is_live,
+                                                       was_live=x.was_live,
+                                                       language=x.language,
+                                                       n_entries=x.n_entries,  # huh,
+                                                       thumbnail=x.thumbnail,
+                                                       upload_date=datetime.datetime.fromtimestamp(x.timestamp))  # is this right
+                                        for x in dlpin.entries],
+                               extractor=models.DLPIE(extractor_key=dlpin.extractor_key,
+                                                      extractor=dlpin.extractor))
